1f96d26c2810ac7117a055dc0e5ca659bc41ba38
Ivan Gankevich <igankevich@yandex.com>
2015-05-11 20:20:36 +0300

    Added sub menus.

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,3 +1,14 @@
 require 'jekyll/multiple/languages/plugin'
 require 'csl/styles'
 require_relative 'jekyll-scholar-hacked/jekyll/scholar'
+
+module Jekyll
+  module BibliographyFilter
+    def superscript_numerals(input)
+	  input.gsub!(/([0-9])(th|st|nd|rd)/, '\1<sup>\2</sup>')
+	  input
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::BibliographyFilter)
