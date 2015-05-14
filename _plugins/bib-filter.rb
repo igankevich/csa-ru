d20c8991704714913eb11d4be4970e6dda157148
Ivan Gankevich <igankevich@yandex.com>
2015-05-14 20:48:49 +0300

    Added citations in bibtex format.

--- _plugins/bib-filter.rb
+++ _plugins/bib-filter.rb
@@ -4,6 +4,27 @@ module Jekyll
 	  input.gsub!(/([0-9])(th|st|nd|rd)/, '\1<sup>\2</sup>')
 	  input
     end
+	def escape_liquid(input)
+	  input.gsub!(/\{\{/, '{2f62314e{')
+	  input.gsub!(/\}\}/, '}2f62314e}')
+	  input
+	end
+	def unescape_liquid(input)
+	  input.gsub!(/2f62314e/, '')
+	  input
+	end
+	def prune_author(input)
+	  input.gsub!(/,/, '')
+	  input.gsub!(/ and /, ', ')
+	  input
+	end
+  end
+  module Search
+    class SearchIndexGenerator < Generator
+	  def generate(site)
+	    docs = site.pages.detect {|page| page.name == 'docs.json'}
+	  end
+	end
   end
 end
 
