a69e5b0a64621357f2295ee60990742ce51bd174
Ivan Gankevich <igankevich@yandex.com>
2015-05-17 13:54:34 +0300

    Converted old site.

--- bin/convert-children.sh
+++ bin/convert-children.sh
@@ -4,6 +4,12 @@ tmp=$(mktemp)
 for file in "$@"; do
 if ! grep 'layout: children' $file >/dev/null 2>&1; then
 title=$(sed -rne 's:\s*<title>\s*(.*)\s*</title>.*:\1:p' $file)
+sed -rne '/<!--\s+Тело/,/<!--\s+@@/p' $file
+echo ""
+echo "File: $file"
+echo "Is this OK?"
+read ans
+if [ "$ans" = "y" ]; then
 {
 cat << EOF
 ---
@@ -14,5 +20,6 @@ EOF
 sed -rne '/<!--\s+Тело/,/<!--\s+@@/p' $file
 } > $tmp
 mv $tmp $file
+fi
 fi
 done
